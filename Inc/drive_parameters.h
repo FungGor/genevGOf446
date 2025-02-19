e92bc40c4731cc0895207295f7b0b4b8b000bbc3
FungGor <kifungleung19970611@gmail.com>
2025-02-19 17:05:48 +0800

    PWM Frequency and CONTROL LOOP (TEST FOT DC RIPPLE)

--- Inc/drive_parameters.h
+++ Inc/drive_parameters.h
@@ -55,7 +55,7 @@
 /**************************    DRIVE SETTINGS SECTION   **********************/
 /* PWM generation and current reading */
 
-#define PWM_FREQUENCY   16000
+#define PWM_FREQUENCY   23000
 #define PWM_FREQ_SCALING 1
 
 #define LOW_SIDE_SIGNALS_ENABLING        LS_PWM_TIMER
@@ -67,18 +67,18 @@
 #define REGULATION_EXECUTION_RATE     1    /*!< FOC execution rate in
                                                            number of PWM cycles */
 /* Gains values for torque and flux control loops */
-#define PID_TORQUE_KP_DEFAULT         2394
-#define PID_TORQUE_KI_DEFAULT         422
+#define PID_TORQUE_KP_DEFAULT         2394  //2394
+#define PID_TORQUE_KI_DEFAULT         422   // 422
 #define PID_TORQUE_KD_DEFAULT         100
-#define PID_FLUX_KP_DEFAULT           2394
-#define PID_FLUX_KI_DEFAULT           422
+#define PID_FLUX_KP_DEFAULT           2394  //2394
+#define PID_FLUX_KI_DEFAULT           422   //422
 #define PID_FLUX_KD_DEFAULT           100
 
 /* Torque/Flux control loop gains dividers*/
-#define TF_KPDIV                      1024
+#define TF_KPDIV                      1024  //1024
 #define TF_KIDIV                      16384
 #define TF_KDDIV                      8192
-#define TF_KPDIV_LOG                  LOG2(1024)
+#define TF_KPDIV_LOG                  LOG2(1024) //change back to 1024
 #define TF_KIDIV_LOG                  LOG2(16384)
 #define TF_KDDIV_LOG                  LOG2(8192)
 #define TFDIFFERENTIAL_TERM_ENABLING  DISABLE
