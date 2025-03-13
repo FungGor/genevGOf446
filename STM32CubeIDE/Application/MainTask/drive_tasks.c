a18e07c5909fb03b0855161f982c2d0a58bbd031
FungGor <kifungleung19970611@gmail.com>
2025-03-13 12:11:33 +0800

    1) Software Hardbraking is removed 2) Motor Temp Sensor is implemented

--- STM32CubeIDE/Application/MainTask/drive_tasks.c
+++ STM32CubeIDE/Application/MainTask/drive_tasks.c
@@ -207,11 +207,6 @@ void GeneralTasks(void const * argument)
 
 			if(*ptr_error_report == 0x00)
 			{
-				/*E-SCOOTER is fully stopped if no any IQ is received*/
-				/*Throttle error happens in the following situations:
-				 * 1) PWM Common mode noise in 50% duty cycle might couples with throttle signal although the motor is "STOPPED" (Partial Stop)
-				 * 2) Voltage Transient which might randomly interfere the throttle signal affecting signal level stability.
-				 * */
 				throttleSignalInput();
 			}
 
